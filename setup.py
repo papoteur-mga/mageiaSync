325f82ab0051a00819e96f87f856e69b42c6a32d
papoteur-mga <yves.brungard_git@gadz.org>
2014-09-10 18:42:53 +0200

    Proposed another setup.py

--- setup.py
+++ setup.py
@@ -1,48 +1,33 @@
-#!/usr/bin/env python
+from distutils.core import setup
+import os
 
-from os import environ
-from os.path import abspath, dirname, join
-from setuptools import setup
-from sys import path as sys_path
+LOCALE_DIR= '/usr/share/locale'
 
-install_requires = [#"QtCore",
-                    #"QtWidgets",
-                    #"QtGui"
-                    ]
+locales = []
+if os.path.exists('po/locale'):
+    for lang in os.listdir('po/locale'):
+        locales.append(os.path.join(lang, 'LC_MESSAGES'))
 
 data_files = [("share/applications/", ["share/applications/mageiasync.desktop"]),
               ("share/icons/hicolor/scalable/apps", ["share/icons/mageiasync.svg"])
-              ]
+              ] + [(os.path.join(LOCALE_DIR, locale),
+                            [os.path.join('po', 'locale', locale, 'mageiasync.mo')])
+                            for locale in locales]
 
-srcdir = join(dirname(abspath(__file__)), "mageiasync/")
-sys_path.insert(0, srcdir)
+setup(
+        name = 'mageiasync',
+        version = '0.1',
+        packages = ['mageiasync'],
+        scripts = ['mageiasync'],
+        license = 'GNU General Public License v3 (GPLv3)',
+        url = 'https://github.com/papoteur-mga/mageiaSync',
+        description = 'This tool downloads ISO images from mirror or Mageia testing server.',
+        long_description = 'This tool uses rsync with a GUI',
+        platforms = ['Linux'],
+        author = 'Papoteur',
+        author_email = 'papoteur@mageialinux-online.org',
+        maintainer = 'david_david',
+        maintainer_email = 'david.david@mageialinux-online.org',
+        data_files = data_files,
+        )
 
-def readme():
-    with open("README.rst") as f:
-        return f.read()
-
-setup(name="mageiasync",
-      version="0.1",
-      description="A frontend to rsync for Mageia usage",
-      long_description=readme(),
-      author="Papoteur",
-      author_email="papoteur@mageialinux-online.org",
-      url="https://github.com/papoteur-mga/mageiaSync",
-      license="GPLv3",
-      platforms=["Linux"],
-#      keywords=['encoding', 'i18n', 'xml'],
-      classifiers=["Development Status :: 5 - Production/Stable",
-                   "Intended Audience :: Developers",
-                   "License :: OSI Approved :: GNU GENERAL PUBLIC LICENSE Version 3 (GPLv3)",
-                   "Operating System :: OS Independent",
-                   "Programming Language :: Python :: 2.6",
-                   "Programming Language :: Python :: 2.7",
-                   "Programming Language :: Python :: 3.3",
-                   "Programming Language :: Python :: 3.4",
-                   "Topic :: Software Development :: Libraries :: Python Modules"],
-      install_requires=install_requires,
-      data_files=data_files,
-      packages=["mageiasync"],
-      py_modules=["mageiasync.mageiasync"],
-      entry_points={"console_scripts": ["mageiasync=mageiasync.mageiasync:main"]}
-      )
